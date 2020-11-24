3ecfefb61875e96a9b2d9fc49c4619edea3632c3
theAndreas <a.burnickl@gmail.com>
2020-11-24 21:02:09 +0100

    Added complete atmel studio project

---
+++ AtmelStudio/TimerTwo/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 24.11.2020 20:49:10
+ * Author : aburn
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
---
+++ AtmelStudio/TimerTwo/TimerTwo/Sketch.cpp
@@ -0,0 +1,18 @@
+﻿/*Begining of Auto generated code by Atmel studio */
+#include <Arduino.h>
+
+/*End of auto generated code by Atmel studio */
+
+
+//Beginning of Auto generated function prototypes by Atmel Studio
+//End of Auto generated function prototypes by Atmel Studio
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
