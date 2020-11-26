07f7391b2ccb3480eb47d7d749da07f5c8a6603b
theAndreas <a.burnickl@gmail.com>
2020-11-26 14:18:12 +0100

    Extended Sketch by demonstration of read method

--- Sketch.ino
+++ Sketch.ino
@@ -1,30 +1,46 @@
 #include <TimerTwo.h>
 
 /*
+/*
  This example toggles the PIN13 cyclically all 1ms and starts the PWM for PIN3.
- The frequency of the PWM depends on the period of the timer.
+ The frequency of the PWM depends on the period of the timer. Furthermore the
+ use of the read method is demonstrated.
 */
 
 #define PIN_TOGGLE          13u
 
+
 void timerCallback() {
     digitalWrite(PIN_TOGGLE, !digitalRead(PIN_TOGGLE));
-};
+}
 
 void setup() {
-  pinMode(PIN_TOGGLE, OUTPUT);
-  if(Timer2.init(Timer2.getPeriodMax(), timerCallback) == E_NOT_OK) {
-	  // something went wrong, check your parameters
-      while(1);
-  }
-  if(Timer2.start() == E_NOT_OK) {
-	  // something went wrong, has init function already been called?
-      while(1);
-  }
-  if(Timer2.enablePwm(TimerTwo::PWM_PIN_3, 127) == E_NOT_OK) {
-      // something went wrong, has init function already been called?
-      while(1);
-  }
+    pinMode(PIN_TOGGLE, OUTPUT);
+    if(Timer2.init(Timer2.getPeriodMax(), timerCallback) == E_NOT_OK) {
+	    // something went wrong, check your parameters
+        while(1);
+    }
+    if(Timer2.enablePwm(TimerTwo::PWM_PIN_3, 127) == E_NOT_OK) {
+        // something went wrong, has init function already been called?
+        while(1);
+    }
+    if(Timer2.start() == E_NOT_OK) {
+        // something went wrong, has init function already been called?
+        while(1);
+    }
+    
+    // Delay of 20ms
+    delay(20u);
+    TimerTwo::TimeType elapsedTime{0u};
+
+    if(Timer2.read(elapsedTime) == E_NOT_OK) {
+        // something went wrong, has init function already been called?
+        while(1);
+    }
+    
+    // Prints the measured time in µs since the last rollover of the timer.
+    // In this example the timer had no rollover until now.
+    Serial.println(elapsedTime);
 }
 
 void loop() {
